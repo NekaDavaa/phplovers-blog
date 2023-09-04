317ea39b1870266bb842bb2c152fbf47e6279668
NekaDavaa <114173828+NekaDavaa@users.noreply.github.com>
2023-09-04 16:11:17 +0300

    Fetched data in admin panel

--- admin/index.php
+++ admin/index.php
@@ -8,26 +8,54 @@
 		<th>Author</th>
 		<th>Date</th>
 	</tr>
+	
+<?php 
+$db = new Database();
+$query = "select p.*, c.name from posts as p inner join categories as c on p.category = c.id";
+$posts = $db->select($query);
+?>
+ <?php while ($row = $posts->fetch_assoc()) : ?>
 	<tr>
-		<td></td>
-		<td></td>
-		<td></td>
-		<td></td>
-		<td></td>
+		<td><?php echo $row['id'];?></td>
+		<td><?php echo $row['title'];?></td>
+		<td><?php echo $row['name'];?></td>
+		<td><?php echo $row['author'];?></td>
+		<td><?php echo $row['date'];?></td>
 	</tr>
+    <?php endwhile; ?>
+
+
 </table>
 
 
+
+
+<?php 
+$db = new Database();
+$query = "select * from categories";
+$categories = $db->select($query);
+?>
 <table class="table table-striped">
 	<tr>
 		<th>Category ID#</th>
 		<th>Category Name</th>
 	</tr>
-	<tr>
-		<td></td>
-		<td></td>
+        <?php while ($row = $categories->fetch_assoc()) : ?>
+    <tr>
+		<td><?php echo $row['id'];?></td>
+		<td><?php echo $row['name'];?></td>
 	</tr>
+
+ 	 <?php endwhile; ?>
+
 </table>
+
+
+
+
+
+
+
 
 <?php include 'includes/footer.php'; ?>	
 	     
