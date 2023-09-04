888ef2b7ea93f0462620c8c3d59e8b4d3414ecd0
NekaDavaa <114173828+NekaDavaa@users.noreply.github.com>
2023-09-04 10:16:15 +0300

    format text
    trim function

--- helpers/format_helper.php
+++ helpers/format_helper.php
@@ -1,8 +1,12 @@
 <?php
 
-function FormatText($fetched_date) {
+function FormatDate($fetched_date) {
 	$timestamp = strtotime($fetched_date);
 	return date("d/m/y", $timestamp);
+}
+
+function FormatText($trim_text) {
+   return mb_strimwidth("$trim_text", 0, 350, "...");
 }
 
 ?>
--- index.php
+++ index.php
@@ -16,8 +16,8 @@
 <?php while ($row = mysqli_fetch_assoc($posts)) : ?>
 <div class="blog-post">
             <h2 class="blog-post-title"><?php echo $row['title'];?></h2>
-            <p class="blog-post-meta"><?php echo FormatText($row['date']) ;?> by <a href="#"><?php echo $row['author']; ?></a></p>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pretium commodo felis vel ultrices. Etiam id dui eros. Praesent nunc dui, volutpat at eleifend nec, suscipit a sapien. Morbi leo urna, aliquam a purus eget, tempus cursus nisl. In hac habitasse platea dictumst. Aliquam pulvinar at lorem vel vulputate. Nunc tempus enim neque. Fusce justo nibh, volutpat eget auctor et, malesuada quis odio. Suspendisse convallis consequat lectus, ullamcorper consequat mauris luctus in. Suspendisse accumsan lorem varius tincidunt tristique. Ut nulla libero, feugiat vitae dui ac, dictum adipiscing libero. Sed sit amet augue mi. Nullam luctus ligula ultricies erat volutpat scelerisque. Etiam molestie sodales aliquam. Donec nisl odio, fringilla quis nibh quis, pulvinar ornare nibh. Integer lorem ligula, scelerisque ut felis et, pretium ultricies arcu.</p>
+            <p class="blog-post-meta"><?php echo FormatDate($row['date']) ;?> by <a href="#"><?php echo $row['author']; ?></a></p>
+				<p><?php echo FormatText($row['body']);?></p>
            <a class="readmore" href="post.php?id=1">Read More</a>
           </div><!-- /.blog-post -->
           <?php endwhile ?>
