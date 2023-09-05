0161da1b7ae1f7e0113966566b6e03c7a89ed7c0
NekaDavaa <114173828+NekaDavaa@users.noreply.github.com>
2023-09-05 17:03:37 +0300

    added some checks

--- posts.php
+++ posts.php
@@ -1,32 +1,40 @@
 <?php include 'includes/header.php'; ?>
+
 <?php
   //Create DB Object
   $db = new Database();
 
-
-if(isset($_GET['category'])) {
-   $category = $_GET['category'];
-   $query = "SELECT * FROM POSTS where category = $category order by date desc"; 
-   //Run query
-   $posts = $db->select($query);
-}
-else {
-  //All posts query 
-  $query = "SELECT * FROM POSTS order by date desc"; 
-  //Run query
-  $posts = $db->select($query);
-}
-
-
+  if(isset($_GET['category'])) {
+     $category = mysqli_real_escape_string($db->link, $_GET['category']); // Sanitize input
+     $query = "SELECT * FROM POSTS WHERE category = '$category' ORDER BY date DESC"; 
+     //Run query
+     $posts = $db->select($query);
+  } else {
+    //All posts query 
+    $query = "SELECT * FROM POSTS ORDER BY date DESC"; 
+    //Run query
+    $posts = $db->select($query);
+  }
 ?>
 
-<?php while ($row = mysqli_fetch_assoc($posts)) : ?>
-<div class="blog-post">
-            <h2 class="blog-post-title"><?php echo $row['title'];?></h2>
-            <p class="blog-post-meta"><?php echo FormatDate($row['date']) ;?> by <a href="#"><?php echo $row['author']; ?></a></p>
-        <p><?php echo FormatText($row['body']);?></p>
-           <a class="readmore" href="post.php?id=<?php echo urlencode($row['id']);?>">Read More</a>
-          </div><!-- /.blog-post -->
-          <?php endwhile ?>
-      
+<?php 
+if ($posts && mysqli_num_rows($posts) > 0) {
+    while ($row = mysqli_fetch_assoc($posts)) : ?>
+        <div class="blog-post">
+            <h2 class="blog-post-title"><?php echo $row['title']; ?></h2>
+            <p class="blog-post-meta"><?php echo FormatDate($row['date']); ?> by <a href="#"><?php echo $row['author']; ?></a></p>
+            <p><?php echo FormatText($row['body']); ?></p>
+            <a class="readmore" href="post.php?id=<?php echo urlencode($row['id']); ?>">Read More</a>
+        </div><!-- /.blog-post -->
+    <?php endwhile; 
+} 
+else {
+    if (isset($category)) {
+        echo "No posts in this category yet";
+    } else {
+        echo "No posts yet";
+    }
+}
+?>
+
 <?php include 'includes/footer.php'; ?>
