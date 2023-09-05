6dd7ee17810f6af0a6e6b2aea64cb005b2e76786
NekaDavaa <114173828+NekaDavaa@users.noreply.github.com>
2023-09-05 16:40:06 +0300

    Added  add category feature

--- admin/add_category.php
+++ admin/add_category.php
@@ -1,8 +1,24 @@
 <?php include 'includes/header.php'; ?>
+<?php
+if (isset($_POST['submit'])){
+$entered_category = $_POST['cat-name'];
+$query = "INSERT INTO categories (`name`) VALUES ('$entered_category')";
+$insert_row = $mysqli->query($query) or die;
+if ($insert_row) {
+    echo '<p class="post-added" style="color:#0cc50c;background: #555050;padding: 5px;display: inline;font-size: 26px;">Category added</p>';
+}
+if (empty($_POST['cat-name'])) {
+        header("Location:add_category.php?error=Please+fill+category+name");
+    }
+}
+?>
+<?php
+if (isset($_GET['error'])) {
+echo "<p class='text-danger'>" . htmlentities($_GET['error']) . "</p>"; }?>
 <form role="form" method="post" action="add_category.php">
   <div class="form-group">
     <label>Category Name</label>
-    <input name="name" type="text" class="form-control" placeholder="Enter Category">
+    <input name="cat-name" type="text" class="form-control" placeholder="Enter Category">
   </div>
   <div>
   <input name="submit" type="submit" class="btn btn-default" value="Submit" />
